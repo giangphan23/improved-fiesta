ea582b73ae405e48b3988a5660924a9b07abe41d
giang <giangphan@docosan.com>
2022-10-26 11:44:17 +0700

    update test.py

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import docosan_module as domo
 
-df = domo.load_gsheet('https://docs.google.com/spreadsheets/d/1ofUHnklPsz3TjImPqIpc7ctPMDHsxRmNGv9DQRRrduo/edit#gid=0')
+df = domo.load_gsheet('https://docs.google.com/spreadsheets/d/1Up9fEqDPEj7AIsPz4qG9KssoDR04PIZzGBRDF_li5LE/edit#gid=0')
 df.head()
 
-domo.update_gsheet('https://docs.google.com/spreadsheets/d/1_o-6K3yaRUGeyUSeySaPG2HS2zwus7HZN1cZqQ6BVKE/edit#gid=0', df.head())
-
-# done
-# clone finished!
